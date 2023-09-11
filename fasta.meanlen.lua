b4fab95dcc8e9d278d5e5be8ce6555f5c6c57040
janxkoci <janxkoci@gmail.com>
2023-09-11 22:27:03 +0200

    switch to local variables

--- fasta.meanlen.lua
+++ fasta.meanlen.lua
@@ -1,6 +1,4 @@
-n = 0
-l = 0
-
+local n, l = 0, 0
 for line in io.lines() do
     if (string.find(line, ">")) then
         n = n + 1
@@ -8,6 +6,4 @@ for line in io.lines() do
         l = l + #line -- string.len(line)
     end
 end
-
 print(l/n)
-

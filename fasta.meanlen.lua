2198d550bcd2cda3c1d1a1a4e3d18f6e8119b954
Jan Koci <janxkoci@gmail.com>
2023-09-25 18:03:36 +0200

    performance tweak

--- fasta.meanlen.lua
+++ fasta.meanlen.lua
@@ -1,6 +1,6 @@
 local n, l = 0, 0
 for line in io.lines() do
-    if (string.find(line, ">")) then
+    if (line:sub(1, 1) == '>') then -- line:sub(1, 1) == '>'; string.find(line, ">")
         n = n + 1
     else
         l = l + #line -- string.len(line)
